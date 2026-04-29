c4cb0c7532936a49e48664a1558eb91ac2bdb174
wuyuanqing <yuan.qing.wu@huawei.com>
2026-04-29 09:08:03 +0800

    Update config

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=/home/models/Kolors/text_encoder:$PYTHONPATH
-torchrun --nproc_per_node=8 train.py --config configs/train/kolors.yaml 2>&1 | tee train.lo
+torchrun --nproc_per_node=8 train.py --config configs/train/kolors.yaml 2>&1 | tee train.log
